ec5cffad663cbe582158061c94e1e755d81514d5
Jackson Eckert <jaxon.eckert@gmail.com>
2024-11-29 16:11:20 -0600

    Add 60FPS framerate clock

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
     pygame.init()
     print("Starting asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -16,6 +18,7 @@ def main():
                 return
         screen.fill(color=(0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
